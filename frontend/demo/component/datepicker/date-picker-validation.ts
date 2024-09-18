58fafacc957ccd9d5e6c9259ceee7a2572c2c10d
Sergey Vinogradov <mr.vursen@gmail.com>
2024-09-18 14:33:08 +0400

    add error messages to DatePicker examples

--- frontend/demo/component/datepicker/date-picker-validation.ts
+++ frontend/demo/component/datepicker/date-picker-validation.ts
@@ -3,7 +3,7 @@ import 'Frontend/demo/init'; // hidden-source-line
 import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import '@vaadin/date-picker';
-import type { DatePickerChangeEvent } from '@vaadin/date-picker';
+import type { DatePicker, DatePickerValidatedEvent } from '@vaadin/date-picker';
 import { applyTheme } from 'Frontend/generated/theme';
 import { addDays, formatISO, isAfter, isBefore } from 'date-fns';
 import dateFnsParse from 'date-fns/parse';
@@ -32,12 +32,19 @@ export class Example extends LitElement {
       <vaadin-date-picker
         label="Appointment date"
         helper-text="Must be within 60 days from today"
+        required
         .min="${formatISO(this.minDate, { representation: 'date' })}"
         .max="${formatISO(this.maxDate, { representation: 'date' })}"
         .errorMessage="${this.errorMessage}"
-        @change="${({ target }: DatePickerChangeEvent) => {
-          const date = dateFnsParse(target.value ?? '', 'yyyy-MM-dd', new Date());
-          if (isBefore(date, this.minDate)) {
+        @validated="${(event: DatePickerValidatedEvent) => {
+          const field = event.target as DatePicker;
+          const date = dateFnsParse(field.value ?? '', 'yyyy-MM-dd', new Date());
+          const inputElement = field.inputElement as HTMLInputElement;
+          if (!field.value && inputElement.value) {
+            this.errorMessage = 'Invalid date format';
+          } else if (!field.value) {
+            this.errorMessage = 'Field is required';
+          } else if (isBefore(date, this.minDate)) {
             this.errorMessage = 'Too early, choose another date';
           } else if (isAfter(date, this.maxDate)) {
             this.errorMessage = 'Too late, choose another date';
